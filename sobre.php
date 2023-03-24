1bee4ec1d158d24dd0583e7a68c84cc79c322be6
Guilherme-Duartee <115901534+Guilherme-Duartee@users.noreply.github.com>
2023-03-24 19:58:43 -0300

    sobre

---
+++ sobre.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link rel="shortcut icon" href="./img/logo.png" type="image/x-icon">
+
+    <!--icons-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+    <!--boostrpap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+    <!--custom css-->
+    <link rel="stylesheet" href="./css/global.css">
+    <link rel="stylesheet" href="./css/home.css">
+    <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href="./css/sobre.css">
+    
+  </head>
+  <body>
+    <?php include './includes/header.php';?>
+    <section>
+            <h1>
+                Sobre a Carioca-Tech
+            </h1>
+
+            <p>
+            Fundada em 2020 por 5 estudantes de informática, a Carioca Tech é uma empresa de “e-commerce” especializada em produtos eletrônicos. Possuímos grande diversidade de produtos, e nossos preços são bastante atraentes.
+            </p>
+
+            <p>
+            Trabalhamos sem cessar para sempre atender nossos clientes com respeito, qualidade e agilidade. 
+            </p>
+
+            <p>
+            Investimos em segurança e somos uma loja confiável com dezenas de milhares pedidos atendidos em todos os estados do Brasil.
+            </p>
+
+            <p>
+            Nosso centro de operações está localizado na cidade do Rio de Janeiro e possui tecnologia avançada para garantir entrega com grande rapidez e exatidão.
+            </p>
+
+    </section>
+
+    <img src="./img/imagem do sobre.webp">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+  </body>
+</html>
