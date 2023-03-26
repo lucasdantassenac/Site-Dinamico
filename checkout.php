6a2af4f3fc2985a722939dcc7d19c0c1d5120455
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-03-26 09:12:22 -0300

    checkout

--- checkout.php
+++ checkout.php
@@ -11,6 +11,7 @@ if(ISSET($_GET['id'])){
   $id = $_GET['id'];
   $sql = "SELECT * FROM produtos WHERE id_produto = '$id';";
   $query = mysqli_fetch_array(mysqli_query($conexao, $sql));
+  $img = $query['foto'];
 }
 ?>
   <body class="bg-light">
@@ -23,48 +24,18 @@ if(ISSET($_GET['id'])){
             <span class="badge badge-secondary badge-pill">3</span>
           </h4>
           <ul class="list-group mb-3">
+            <img src='./img/<?= $img ?>' class='list-group-item w-100' alt='imagem do produto'>
             <li class="list-group-item d-flex justify-content-between lh-condensed">
               <div>
-                <h6 class="my-0">Product name</h6>
-                <small class="text-muted">Brief description</small>
+                <h6 class="my-0"><?= $query['nome']?></h6>
               </div>
-              <span class="text-muted">$12</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">Second product</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$8</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">Third item</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$5</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between bg-light">
-              <div class="text-success">
-                <h6 class="my-0">Promo code</h6>
-                <small>EXAMPLECODE</small>
-              </div>
-              <span class="text-success">-$5</span>
+              <span class="text-muted">R$ <?= $query['preco']?>,00</span>
             </li>
             <li class="list-group-item d-flex justify-content-between">
-              <span>Total (USD)</span>
-              <strong>$20</strong>
+              <span>Total:</span>
+              <strong>R$ <?= $query['preco']?>,00</strong>
             </li>
           </ul>
-
-          <form class="card p-2">
-            <div class="input-group">
-              <input type="text" class="form-control" placeholder="Promo code">
-              <div class="input-group-append">
-                <button type="submit" class="btn btn-secondary">Redeem</button>
-              </div>
-            </div>
-          </form>
         </div>
         <div class="col-md-8 order-md-1">
           <h4 class="mb-3">Página de compra</h4>
